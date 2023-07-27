c6115eabd1a3f22ddb2d29623bc520ae27e32f57
Farzanafasil <121241440+Farzanafasil@users.noreply.github.com>
2023-07-27 21:24:58 +0400

    router file authentication by farzana

--- routes/Curriculum.js
+++ routes/Curriculum.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
 const jwt =require("jsonwebtoken");
 const curriculumDATA = require('../model/curriculum')
-router.get('/curriculumlist',async(req,res)=>{
+
+
+router.get('/curriculumlist/:token',async(req,res)=>{
+    let data = await curriculumDATA.find()
     try {
-        let data = await curriculumDATA.find()
-        res.send(data)
+      jwt.verify(req.params.token,"ict",
+      (error,decoded)=>{
+        if(decoded && decoded.email){
+            res.send(data);
+        }
+        else
+        {
+            res.json({message:"Unauthorized user"})
+        }
+      })
+       
     } catch (error) {
         res.send(error.message)
     }
@@ -23,42 +35,58 @@ router.post('/curriculumlist',async(req,res)=>{
         console.log(req.body)
         const { requirementname, area, institution ,category, hours, admin_upload_url ,status} = req.body;
         const curriculum = await curriculumDATA({ requirementname, area, institution ,category, hours, admin_upload_url ,status});
-        curriculum.save()  ;
-        res.json({message:"Created Succesfully"});
-        // jwt.verify(req.body.token,"ict",(error,decoded)=>{
-        //     if (decoded && decoded.email) {
+        // curriculum.save()  ;
+        // res.json({message:"Created Succesfully"});
+        jwt.verify(req.body.token,"ict",(error,decoded)=>{
+            if (decoded && decoded.email) {
                 
-        //         curriculum.save()  ;
-        //         res.json({message:"Created Succesfully"});
+                curriculum.save()  ;
+                res.json({message:"Created Succesfully"});
                 
-        //     } else {
-        //         res.json({message:"Unauthorised User"});
+            } else {
+                res.json({message:"Unauthorised User"});
                 
-        //     }
+            }
     
-        //    })
+           })
         
     } catch (error) {
         console.log(error)
         res.json('error')
     }
 })
-router.put('/curriculumlist/:id',async(req,res)=>{
+router.put('/curriculumlist/:id',(req,res)=>{
     try {
-       id = req.params.id
-       let updateData = {$set:req.body}
-       const updated = await curriculumDATA.findByIdAndUpdate(id, updateData)
-        res.json({message:"Updated successfully"})
+        jwt.verify(req.body.token,"ict",(error,decoded)=>{
+            if (decoded && decoded.email) {
+                
+                id = req.params.id
+                let updateData = {$set:req.body}
+                const updated = curriculumDATA.findByIdAndUpdate(id, updateData)
+                 res.json({message:"Updated successfully"})
+                
+            } else {
+                res.json({message:"Unauthorised User"});
+                
+            }
+    
+           })
+
+      
     } catch (error) {
         // console.log(error)
         res.send('error')
     }
 })
-router.delete('/curriculumlist/:id',async(req,res)=>{
+router.delete('/curriculumlist/:id',async (req,res)=>{
     try {
-        let id = req.params.id
-       const updated = await curriculumDATA.findByIdAndDelete(id)
-       res.json({message:"Deleted successfully"})
+        
+                
+                let id = req.params.id
+                 const updated = await curriculumDATA.findByIdAndDelete(id)
+                res.json({message:"Deleted successfully"})
+          
+        
     } catch (error) {
         console.log(error)
         res.send('error')
